cd065a0ab541408120d6d7c8316914e5e152fb18
ZenonStella <stellazenon@gmail.com>
2022-05-10 11:18:55 +0200

    correction orthographique

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,26 +6,26 @@ let sign = ['pierre', 'papier', 'ciseaux']
 // compteur
 let countSuccessGames = 0;
 
-// fonction qui recupère mon chois via les btns dans le html
+// fonction qui récupère mon choix via les btns dans le html
 function clickBtn(id) {
     // variable 
     let successGames = document.getElementById('successGames')
-    // variable du chiffre aleatiore
-    let aleatorNumber = Math.floor(Math.random() * 3);
+    // variable du chiffre aléatoire
+    let randomNumber = Math.floor(Math.random() * 3);
     //  j'attribut le chiffre aleatoire a l'index du tableau et je stock la valeur dans un variable
-    let ordiChoise = sign[aleatorNumber];
-    //  j'affiche dans le html, le chois de l'ordi et le mien
+    let ordiChoise = sign[randomNumber];
+    //  j'affiche dans le html, le choix de l'ordi et le mien
     text.innerHTML = `<p>Vous avez choisi ${id} et l'ordi a choisi ${ordiChoise}</p>`;
-    // condition si les deux valeurs sont identique
+    // condition si les deux valeurs sont identiques
     if (id == ordiChoise) {
         text.innerHTML += '<p id="resultNull">égalité</p>';
     } else if (id == 'pierre' && ordiChoise == 'papier' || id == 'papier' && ordiChoise == 'ciseaux' || id == 'ciseaux' && ordiChoise == 'pierre') {
-        // condition de tout les cas ou je perd
+        // condition de tous les cas où je perds
         text.innerHTML += '<p id="resultLost">perdu</p>';
     } else {
         text.innerHTML += '<p id="resultSuccsses">bravo</p>';
         document.getElementById('resultSuccsses').style.backgroundColor = 'green';
-        // mon compteur s'incremente de 1 si je gagne
+        // mon compteur s'incrémente de 1 si je gagne
         countSuccessGames++
     }
     
